81ba21a6a54e0319228a1b6ab0b25cfbceecb1d4
HRB <sriramhiren@gmail.com>
2024-12-30 12:07:40 +0530

    updated with render to see the change in refs value

--- src/StateRef.js
+++ src/StateRef.js
@@ -19,19 +19,25 @@ import { Component, useState, useRef } from "react";
 
 const StateRef = () => {
     // const [count, setCount] = useState(0);
+    const [renCount, setRenCount] = useState(0);
     const count = useRef(0); // inital count value as 0
 
     const increment = () => {
         count.current += 1;
         console.log("Ref Update", count.current);
     }
+
+    const renIncrement = () => {
+        setRenCount(prevCount => prevCount + 1);
+        }
     
     console.log("Rendered");
     return (
         <div>
             You have clicked {count.current} number of times!
             <br></br>
-            <button onClick={increment}>Increase with Prev Inline</button>
+            <button onClick={increment}>Ref</button>
+            <button onClick={renIncrement}>Render</button>
         </div>
     );
 }
